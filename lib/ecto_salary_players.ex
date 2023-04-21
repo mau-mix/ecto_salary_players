9e6ec5dfb7ef80d3ca39bd0e3b5fa94d239aa6d3
Mauricio <m.musicalmix@gmail.com>
2023-04-20 18:40:16 -0600

    iniciando proyecto

---
+++ lib/ecto_salary_players.ex
@@ -0,0 +1,18 @@
+defmodule EctoSalaryPlayers do
+  @moduledoc """
+  Documentation for `EctoSalaryPlayers`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoSalaryPlayers.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecto_salary_players_test.exs
@@ -0,0 +1,8 @@
+defmodule EctoSalaryPlayersTest do
+  use ExUnit.Case
+  doctest EctoSalaryPlayers
+
+  test "greets the world" do
+    assert EctoSalaryPlayers.hello() == :world
+  end
+end
